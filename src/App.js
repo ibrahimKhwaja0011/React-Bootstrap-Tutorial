93373f0628e89267e94bef9752a6e1f596fc3821
M Ibrahim Khwaja <khwaja.nu@gmail.com>
2019-08-06 21:01:41 +0500

    1.1 Update ByteFuge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home } from './Home';
-import { About } from './About';
-import { Contact } from './Contact';
-import { NoMatch } from './NoMatch';
-import { Layout } from './components/Layout';
-import { NavigationBar } from './components/NavigationBar';
-import { Jumbotron } from './components/Jumbotron';
+import  Home from './Home';
+import  About  from './About';
+import  Contact  from './Contact';
+import  NoMatch  from './NoMatch';
+import  Layout  from './components/Layout';
+import  NavigationBar  from './components/NavigationBar';
+import  Jumbotron  from './components/Jumbotron';
 
 class App extends Component {
   render() {
